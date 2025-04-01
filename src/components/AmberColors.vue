8bd1184ced38fcc42c2e20fabbab87d8eeb36039
co6tter <co6tter@gmail.com>
2025-04-01 13:48:18 +0900

    chore: add AmberColors

---
+++ src/components/AmberColors.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <h1 class="text-3xl">Amber</h1>
+    <div class="flex gap-2">
+      <div class="w-20 h-20 bg-amber-50"></div>
+      <div class="w-20 h-20 bg-amber-100"></div>
+      <div class="w-20 h-20 bg-amber-200"></div>
+      <div class="w-20 h-20 bg-amber-300"></div>
+      <div class="w-20 h-20 bg-amber-400"></div>
+      <div class="w-20 h-20 bg-amber-500"></div>
+      <div class="w-20 h-20 bg-amber-600"></div>
+      <div class="w-20 h-20 bg-amber-700"></div>
+      <div class="w-20 h-20 bg-amber-800"></div>
+      <div class="w-20 h-20 bg-amber-900"></div>
+      <div class="w-20 h-20 bg-amber-950"></div>
+    </div>
+  </section>
+</template>
