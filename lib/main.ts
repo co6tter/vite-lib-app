cd023367445b4e9315b76a8596ed174a2115733b
co6tter <co6tter@gmail.com>
2025-04-03 11:55:34 +0900

    fix: add css import to lib/main.ts

--- lib/main.ts
+++ lib/main.ts
@@ -1,3 +1,5 @@
+import "../src/style.css";
+
 import AmberColors from "../src/components/AmberColors.vue";
 import OrangeColors from "../src/components/OrangeColors.vue";
 import RedColors from "../src/components/RedColors.vue";
